4283ef794fc1c1a373693a9c870b7be9f0aed13e
supaKd <kevin.khek@gmail.com>
2025-07-16 15:35:49 +0200

    footer

--- src/pages/product/Menu.jsx
+++ src/pages/product/Menu.jsx
@@ -30,7 +30,7 @@ function Menu() {
 
       <section className="menu-grid">
         {products.map((cat, index) => (
-          <ScrollRevealWrapper key={cat.id} delay={index * 0.1}>
+          <ScrollRevealWrapper key={cat.id} delay={index * 0.01}>
             {/* on ne touche pas à la structure interne */}
             <Link to={`/menu/${cat.slug}`} className="menu-item">
               <img src={cat.image} alt={cat.name} />
